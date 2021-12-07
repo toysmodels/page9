1c94d923b43c960aff3ce2c46c81dc8ca812597b
toysmodels <74579043+toysmodels@users.noreply.github.com>
2021-12-06 20:16:52 -0500

    Add files via upload

---
+++ productos/constantes_productos.js
@@ -0,0 +1,67 @@
+/***** JEEP *******/
+var nombre_jeep="Jeep 4x4";
+var valor_jeep="170.000";
+var unidades_jeep="10";
+
+
+/***** CHASIS *******/
+var nombre_chasis="Chasis"
+var valor_chasis="100.000"
+var unidades_chasis="7"
+
+
+
+/***** TRAILER *******/
+var nombre_trailer="Trailer"
+var valor_trailer="60.000"
+var unidades_trailer="5"
+
+
+/***** TOYOTA FJ40 *******/
+var nombre_toyota_fj40="TOYOTA_FJ40"
+var valor_toyota_fj40="160.000"
+var unidades_toyota_fj40="0"
+
+
+/***** kit tiro al blanco M9*******/
+var nombre_kit_tiro_M9="kit_Tiro"
+var valor_kit_tiro_M9="62.000"
+var unidades_kit_tiro_M9="12"
+
+/***** Automata flexiones de brazos *******/
+var nombre_automata_flexiones="Automata_flexiones"
+var valor_automata_flexiones="65.000"
+var unidades_automata_flexiones="20"
+
+/***** Gran padoga del Ganso *******/
+var nombre_gran_padoga_ganso="Gran Padoga del Ganso"
+var valor_gran_padoga_ganso="145.000"
+var unidades_gran_padoga_ganso="5"
+
+/***** Yueyang Tower *******/
+var nombre_yueyang_tower="Yueyang Tower"
+var valor_yueyang_tower="155.000"
+var unidades_yueyang_tower="4"
+
+/***** Templo Chino del Cielo *******/
+var nombre_templo_oracion="Templo de Oracion"
+var valor_templo_oracion="130.000"
+var unidades_templo_oracion="2"
+
+/***** Torre Pisa  *******/
+var nombre_torre_pisa="Torre Pisa"
+var valor_torre_pisa="135.000"
+var unidades_torre_pisa="7"
+
+/***** Torre Kuala Lumpur *******/
+var nombre_torre_kuala_lumpur="Torre Kuala Lumpur"
+var valor_torre_kuala_lumpur="75.000"
+var unidades_torre_kuala_lumpur="8"
+
+/***** Burj Arab *******/
+var nombre_burj_arab="Burj Arab"
+var valor_burj_arab="70.000"
+var unidades_burj_arab="6"
+
+
+
