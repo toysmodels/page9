1e82e8ab0ae564a877782910781362f48cc1e94c
toysmodels <74579043+toysmodels@users.noreply.github.com>
2023-02-02 14:52:30 -0500

    Update constantes_productos.js

--- productos/constantes_productos.js
+++ productos/constantes_productos.js
@@ -7,7 +7,7 @@ var unidades_jeep="3";
 
 /***** CHASIS *******/
 var nombre_chasis="Chasis"
-var valor_chasis="65.000"
+var valor_chasis="75.000"
 var unidades_chasis="2"
 
 
@@ -26,40 +26,40 @@ var unidades_toyota_fj40="0"
 
 /***** kit tiro al blanco M9*******/
 var nombre_kit_tiro_M9="kit_Tiro"
-var valor_kit_tiro_M9="65.000"
+var valor_kit_tiro_M9="50.000"
 var unidades_kit_tiro_M9="3"
 
 /***** Automata flexiones de brazos *******/
 var nombre_automata_flexiones="Automata_flexiones"
-var valor_automata_flexiones="35.000"
+var valor_automata_flexiones="50.000"
 var unidades_automata_flexiones="3"
 
 /***** Gran padoga del Ganso *******/
 var nombre_gran_padoga_ganso="Gran Padoga del Ganso"
-var valor_gran_padoga_ganso="90.000"
+var valor_gran_padoga_ganso="80.000"
 var unidades_gran_padoga_ganso="2"
 
 /***** Yueyang Tower *******/
 var nombre_yueyang_tower="Yueyang Tower"
-var valor_yueyang_tower="140.000"
+var valor_yueyang_tower="80.000"
 var unidades_yueyang_tower="2"
 
 /***** Templo Chino del Cielo *******/
 var nombre_templo_oracion="Templo Chino"
-var valor_templo_oracion="90.000"
+var valor_templo_oracion="80.000"
 var unidades_templo_oracion="2"
 
 /***** Torre Pisa  *******/
 var nombre_torre_pisa="Torre Pisa"
-var valor_torre_pisa="90.000"
+var valor_torre_pisa="70.000"
 var unidades_torre_pisa="3"
 
 /***** Torre Kuala Lumpur *******/
 var nombre_torre_kuala_lumpur="Torre Kuala Lumpur"
-var valor_torre_kuala_lumpur="65.000"
+var valor_torre_kuala_lumpur="50.000"
 var unidades_torre_kuala_lumpur="3"
 
 /***** Burj Arab *******/
 var nombre_burj_arab="Burj Arab"
-var valor_burj_arab="70.000"
+var valor_burj_arab="50.000"
 var unidades_burj_arab="3"
