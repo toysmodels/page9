d54e2487d6d79506c49a4b17460fdd659e9b2a7f
toysmodels <74579043+toysmodels@users.noreply.github.com>
2026-01-08 19:49:44 -0500

    Update constantes_productos.js

--- productos/constantes_productos.js
+++ productos/constantes_productos.js
@@ -1,13 +1,13 @@
 
 /***** JEEP *******/
 var nombre_jeep="Jeep 4x4";
-var valor_jeep="120.000";
+var valor_jeep="150.000";
 var unidades_jeep="3";
 
 
 /***** CHASIS *******/
 var nombre_chasis="Chasis"
-var valor_chasis="75.000"
+var valor_chasis="100.000"
 var unidades_chasis="2"
 
 
@@ -20,46 +20,47 @@ var unidades_trailer="2"
 
 /***** TOYOTA FJ40 *******/
 var nombre_toyota_fj40="TOYOTA_FJ40"
-var valor_toyota_fj40="120.000"
+var valor_toyota_fj40="150.000"
 var unidades_toyota_fj40="0"
 
 
 /***** kit tiro al blanco M9*******/
 var nombre_kit_tiro_M9="kit_Tiro"
-var valor_kit_tiro_M9="80.000"
+var valor_kit_tiro_M9="100.000"
 var unidades_kit_tiro_M9="3"
 
 /***** Automata flexiones de brazos *******/
 var nombre_automata_flexiones="Automata_flexiones"
-var valor_automata_flexiones="50.000"
+var valor_automata_flexiones="100.000"
 var unidades_automata_flexiones="3"
 
 /***** Gran padoga del Ganso *******/
 var nombre_gran_padoga_ganso="Gran Padoga del Ganso"
-var valor_gran_padoga_ganso="100.000"
+var valor_gran_padoga_ganso="150.000"
 var unidades_gran_padoga_ganso="2"
 
 /***** Yueyang Tower *******/
 var nombre_yueyang_tower="Yueyang Tower"
-var valor_yueyang_tower="120.000"
+var valor_yueyang_tower="150.000"
 var unidades_yueyang_tower="2"
 
 /***** Templo Chino del Cielo *******/
 var nombre_templo_oracion="Templo Chino"
-var valor_templo_oracion="100.000"
+var valor_templo_oracion="150.000"
 var unidades_templo_oracion="2"
 
 /***** Torre Pisa  *******/
 var nombre_torre_pisa="Torre Pisa"
-var valor_torre_pisa="100.000"
+var valor_torre_pisa="150.000"
 var unidades_torre_pisa="3"
 
 /***** Torre Kuala Lumpur *******/
 var nombre_torre_kuala_lumpur="Torre Kuala Lumpur"
-var valor_torre_kuala_lumpur="80.000"
+var valor_torre_kuala_lumpur="100.000"
 var unidades_torre_kuala_lumpur="3"
 
 /***** Burj Arab *******/
 var nombre_burj_arab="Burj Arab"
-var valor_burj_arab="80.000"
+var valor_burj_arab="100.000"
 var unidades_burj_arab="3"
+
